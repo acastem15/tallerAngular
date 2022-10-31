8e7c2b2b77510d7b85836b0aeb15f84e97a561ba
AnaSofia CastellanosMosquera <a.castellanosm@uniandes.edu.co>
2022-10-30 23:42:25 -0500

    average

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -8,18 +8,31 @@ import { SerieService } from './serie.service';
   styleUrls: ['./serie.component.css']
 })
 export class SerieComponent implements OnInit {
+  average: number = 0;
 
 
   constructor(private serieService:SerieService ) { }
   series:Array<Serie>=[];
+
 
   getSerieList() {
     this.serieService.getSeries().subscribe((ss) => {
       this.series = ss;
     });
   }
+  getAverageSeasons(series:Array<Serie>) {
+    let total = 0
+    let seriesCan=series.length
+    series.forEach(element => {
+      total+=element.seasons
+    });
+
+    this.average= total/seriesCan
+
+  }
   ngOnInit() {
     this.getSerieList();
+    this.getAverageSeasons(this.series);
   }
 
 }
